ae0de1b7b5eef749fc4b1ab7470a2aca79cdf56c
Josh Richards <no-reply@instinctualsoftware.com>
2007-09-04 16:01:59 +0000

    getting django in third_party

--- lib/site.py
+++ lib/site.py
@@ -2,6 +2,6 @@ import os
 import sys
 
 
-libDir = os.sep.join(__file__.split(os.sep)[:-1])
-thirdParty = os.sep.join([libDir, 'third_party'])
+projectDir = os.sep.join(__file__.split(os.sep)[:-2])
+thirdParty = os.sep.join([projectDir, 'third_party', 'lib'])
 sys.path.append(thirdParty)
