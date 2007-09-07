615d4cfec5a68c67d99ab08125732d48572d79cb
Josh Richards <no-reply@instinctualsoftware.com>
2007-09-07 05:00:23 +0000

    changed the base client object back in __init__

--- lib/instinctual/informer/client/__init__.py
+++ lib/instinctual/informer/client/__init__.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python
 
+import restclient
 from xml.dom import minidom, Node
 
+import instinctual
+from instinctual import informer
 from instinctual.informer.client.appevent import *
-from instinctual.informer.client.client import *
+
+LOG = instinctual.getLogger(__name__)
+
+# ------------------------------------------------------------------------------
+class Client(object):
+    def postAppEvent(self, appEvent, async=False):
+        appEventUrl = instinctual.informer.getAppEventUrl()
+        LOG.info(restclient.POST(appEventUrl, appEvent.asDict(), async=async))
+
+    def getNotes(self, setup): 
+        parsed = informer.parseSetup(setup)
+        project = parsed['project']
+        shot = parsed['shot']
+        url = informer.getProjectShotNotesUrl(project, shot)
+        xml = restclient.GET(url)
+        print "Got xml:", xml
+        pass    
+
+    def getElements(self, setup): 
+        parsed = informer.parseSetup(setup)
+        project = parsed['project']
+        shot = parsed['shot']
+        url = informer.getProjectShotElementsUrl(project, shot)
+        xml = restclient.GET(url)
+        print "Got xml:", xml
+        pass    
 
