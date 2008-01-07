0e60c408ad7d8a22d8f74aa21f75c7c65c7af0c3
Josh Richards <no-reply@instinctualsoftware.com>
2008-01-07 06:24:20 +0000

    move third_party/lib -> third_party/python since ffmpeg and x264 now require their own third_party/lib for libraries

--- lib/sitecustomize.py
+++ lib/sitecustomize.py
@@ -4,5 +4,5 @@ import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'instinctual.settings'
 
 projectDir = os.sep.join(__file__.split(os.sep)[:-2])
-thirdParty = os.sep.join([projectDir, 'third_party', 'lib'])
+thirdParty = os.sep.join([projectDir, 'third_party', 'python'])
 sys.path.append(thirdParty)
