e57620ebb924ee1be3496cdda92c50b2f3c7318d
Josh Richards <no-reply@instinctualsoftware.com>
2008-01-08 08:57:50 +0000

    Work around a problem with Python 2.3's version of httplib and POST's with Content-Length headers.
    Manually distribute the Python 2.5's version of
    httplib and update the sitecustomize script to
    prepend the third_party python lib path so our
    httplib will take precedence.

--- lib/sitecustomize.py
+++ lib/sitecustomize.py
@@ -5,4 +5,5 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'instinctual.settings'
 
 projectDir = os.sep.join(__file__.split(os.sep)[:-2])
 thirdParty = os.sep.join([projectDir, 'third_party', 'python'])
-sys.path.append(thirdParty)
+sys.path.insert(0, thirdParty)
+print "path:", sys.path
