460652ef5e18a279afefbbf8eef240ff0f80ff50
Alan Latteri <alan@instinctualsoftware.com>
2008-01-13 02:07:51 +0000

    fixing svn... it doesn't like direcotries of the same name with different capitilization.

--- ui/Flash/scripts.js
+++
@@ -1,6 +0,0 @@
- function renderFlash(src, width, height, features) {
-	document.writeln('<embed src="' + src + '" quality="high" width="' + width + 
-					 '" height="' + height + '" align="middle" allowscriptaccess="sameDomain" ' +
-					 'type="application/x-shockwave-flash" ' + features + 
-  					 'pluginspage="http://www.macromedia.com/go/getflashplayer" />');
-  }
