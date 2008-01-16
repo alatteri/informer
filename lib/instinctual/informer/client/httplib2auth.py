0f89fa29131f91b233a7e970849b48c3f46498d5
Josh Richards <no-reply@instinctualsoftware.com>
2008-01-16 06:45:44 +0000

    cache the http authorizations across multiple objects

--- lib/instinctual/informer/client/httplib2auth.py
+++ lib/instinctual/informer/client/httplib2auth.py
@@ -3,8 +3,14 @@ from httplib2 import *
 
 print "----- httplib2auth loaded ----"
 
+_auths = []
+
 class Http(httplib2.Http):
     def __init__(self, *args, **kwargs):
         print "word up. My __init__ was called"
         httplib2.Http.__init__(self, *args, **kwargs)
         self.add_credentials('spark', 'sparky')
+
+        # used the cached auths
+        global _auths
+        self.authorizations = _auths
