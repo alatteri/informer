07590f3e644342be3bff8f7444807e8d0847eb82
Josh Richards <no-reply@instinctualsoftware.com>
2008-02-16 13:58:55 +0000

    remove all of the fields that are consts or can be determined at run time from the config file. provide them as defaults at constuction.
    only server, proto and port are required now

--- lib/instinctual/__init__.py
+++ lib/instinctual/__init__.py
@@ -8,13 +8,33 @@ rootDir = os.sep.join(__file__.split(os.sep)[:-3])
 confDir = os.path.join(rootDir, 'conf')
 logIni = os.path.join(confDir, 'logging.ini')
 
+CONF_DEFAULTS = {
+    "url_api":                    "informer/1.0",
+    "url_projects":               "projects/",
+    "url_shots":                  "shots/",
+    "url_project_shots":          "projects/%s/shots/",
+    "url_project_shot":           "projects/%s/shots/%s/",
+    "url_project_shot_logs":      "projects/%s/shots/%s/logs/",
+    "url_project_shot_note":      "projects/%s/shots/%s/notes/%s/",
+    "url_project_shot_notes":     "projects/%s/shots/%s/notes/",
+    "url_project_shot_clips":     "projects/%s/shots/%s/renders/",
+    "url_project_shot_events":    "projects/%s/shots/%s/events/",
+    "url_project_shot_frames":    "projects/%s/shots/%s/frames/",
+    "url_project_shot_element":   "projects/%s/shots/%s/elements/%s/",
+    "url_project_shot_elements":  "projects/%s/shots/%s/elements/",
+    "url_users":                  "users/",
+    "dir_logs":                   os.path.join(rootDir, 'logs'),
+    "dir_uploads":                os.path.join(rootDir, 'uploads'),
+    "dir_filelist_base":          "/tmp",
+}
+
+confIni = os.path.join(confDir, 'instinctual.ini')
+conf = ConfigParser.ConfigParser(defaults=CONF_DEFAULTS)
+conf.read(confIni)
+
 logging.config.fileConfig(logIni)
 logging.codecs = codecs.getwriter('utf-8')
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
-
-confIni = os.path.join(confDir, 'instinctual.ini')
-conf = ConfigParser.ConfigParser()
-conf.read(confIni)
 
 def getLogger(name):
     return logging.getLogger(name)
