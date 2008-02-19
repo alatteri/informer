a90e9231a65189f1c96cbbc9ec2d162087349bf1
Josh Richards <no-reply@instinctualsoftware.com>
2008-02-19 00:31:07 +0000

    get view larger working

--- media/js/scripts.js
+++ media/js/scripts.js
@@ -8,4 +8,20 @@
   function renderQuickTimeLauncher(clip) {
 	  document.writeln('<embed src="placeholder.mov" autohref="true" target="quicktimeplayer" controller="false" width="114" height="27" autoplay="true" href="' + clip + '"></embed>');
   }
-  
+
+  function getArgs() {
+    var args = new Array();
+    var query = window.location.search.substring(1);
+    var parms = query.split('&');
+
+    for (var i=0; i<parms.length; i++) {
+      var pos = parms[i].indexOf('=');
+      if (pos > 0) {
+        var key = parms[i].substring(0,pos);
+        var val = parms[i].substring(pos+1);
+        args[key] = val;
+      }
+    }
+
+    return args;
+  }
