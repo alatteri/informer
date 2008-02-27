6aa05d39bd0e2f4987b267d93af9f7edb21b688e
Josh Richards <no-reply@instinctualsoftware.com>
2008-02-27 07:50:48 +0000

    return the object_id of the object that generated the log entry

--- lib/instinctual/informer/models.py
+++ lib/instinctual/informer/models.py
@@ -362,7 +362,7 @@ class Log(InformerMixIn, models.Model):
                         'msg_prefix', 'object_repr', 'msg_suffix')
 
     class Rest:
-        expose_fields = ['who', 'when', 'action', 'type',
+        expose_fields = ['who', 'when', 'action', 'type', 'object_id',
                          'msg_prefix', 'object_repr', 'msg_suffix']
 
 # ------------------------------------------------------------------------------
