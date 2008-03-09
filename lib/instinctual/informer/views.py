03bc160c015946d24a21246cf2423663b0a9e711
Josh Richards <no-reply@instinctualsoftware.com>
2008-03-09 03:05:07 +0000

    pass to the user to all pages, draw the breadcrumb correctly

--- lib/instinctual/informer/views.py
+++ lib/instinctual/informer/views.py
@@ -13,18 +13,22 @@ from instinctual.informer.models import Project, Shot, Note, Render, Element, Lo
 def project_list(request):
     responder = CustomJSONResponder()
     responder.expose_fields = Project.Rest.expose_fields
-    context = dict(
-        object_list = Project.objects.all(),
-        json_url = informer.getProjectsUrl(format='json'),
-        user = request.user,
-        data = responder.render(Project.objects.all()))
-    return render_to_response('informer/project_list.html', context)
+    #    data = responder.render(Project.objects.all()))
+    c = {
+        'user':      request.user,
+        'object_list':  Project.objects.all(),
+    }
+    return render_to_response('informer/project_list.html', c)
 project_list = login_required(project_list)
 
 # ------------------------------------------------------------------------------
 def project_detail(request, project_name):
     p = get_object_or_404(Project, name=project_name)
-    return render_to_response('informer/project_detail.html', {'project':p})
+    c = {
+        'user':     request.user,
+        'project':  p,
+    }
+    return render_to_response('informer/project_detail.html', c)
 project_detail = login_required(project_detail)
 
 # ------------------------------------------------------------------------------
@@ -34,15 +38,13 @@ def shot_logs(request, project_name, shot_name):
     responder = CustomJSONResponder()
     responder.expose_fields = Log.Rest.expose_fields
     c = {
+        'user':     request.user,
         'project':  p,
         'shot':     shot,
-        'title':    'Overview',
-        'user':     request.user,
         'data':     responder.render(shot.log_set.all()),
     }
     return render_to_response('informer/shot_detail_logs.html', c)
 shot_detail = login_required(shot_logs)
-
 
 # ------------------------------------------------------------------------------
 def shot_notes(request, project_name, shot_name):
@@ -52,16 +54,14 @@ def shot_notes(request, project_name, shot_name):
     responder = CustomJSONResponder()
     responder.expose_fields = Note.Rest.expose_fields
     c = {
+        'user':     request.user,
         'project':  p,
         'shot':     shot,
-        'users':    users,
-        'title':    'Notes',
-        'user':     request.user,
         'data':     responder.render(shot.note_set.all()),
+        'users':    users,
     }
     return render_to_response('informer/shot_detail_notes.html', c)
 shot_notes = login_required(shot_notes)
-
 
 # ------------------------------------------------------------------------------
 def shot_elements(request, project_name, shot_name):
@@ -70,15 +70,13 @@ def shot_elements(request, project_name, shot_name):
     responder = CustomJSONResponder()
     responder.expose_fields = Element.Rest.expose_fields
     c = {
+        'user':     request.user,
         'project':  p,
         'shot':     shot,
-        'title':    'Elements',
-        'user':     request.user,
         'data':     responder.render(shot.element_set.all()),
     }
     return render_to_response('informer/shot_detail_elements.html', c)
 shot_elements = login_required(shot_elements)
-
 
 # ------------------------------------------------------------------------------
 def shot_renders(request, project_name, shot_name):
@@ -87,15 +85,13 @@ def shot_renders(request, project_name, shot_name):
     responder = CustomJSONResponder()
     responder.expose_fields = Render.Rest.expose_fields
     c = {
+        'user':     request.user,
         'project':  p,
         'shot':     shot,
-        'title':    'Renders',
-        'user':     request.user,
         'data':     responder.render(shot.render_set.all()),
     }
     return render_to_response('informer/shot_detail_renders.html', c)
 shot_renders = login_required(shot_renders)
-
 
 def limited_object_list(*args, **kwargs):
     return object_list(*args, **kwargs)
